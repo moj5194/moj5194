1f1176f92079a97ec94110747e88fc4101cc564e
moj5194 <moj5194@OZWDLND347-167.win.psu.edu>
2017-04-04 16:57:34 -0400

    deposit class

---
+++ model/src/DepositTransaction.java
@@ -0,0 +1,51 @@
+
+import java.time.LocalDate;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author moj5194
+ */
+class DepositTransaction {
+     private LocalDate date;
+  private String description;
+  private int amount;
+  private int total;
+
+  public LocalDate getDate() {
+      return date;
+   }
+
+   public void setDate(LocalDate date) {
+      this.date = date;
+   }
+
+   public String getDescription() {
+      return description;
+   }
+
+   public void setDescription(String Decription) {
+      this.description = description;
+   }
+   
+   public int getAmount() {
+      return (int) amount;
+   }
+
+   public void setAmount(int amount) {
+      this.amount = amount;
+   }
+
+   public int getTotal() {
+      return total;
+   }
+
+   public void settotal(int total) {
+      this.total = total;
+   } 
+}
